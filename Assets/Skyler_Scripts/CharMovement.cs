3a39ebcd5d0d0af71eb78a700d82ef0752d22e3d
QuickBowl <quickbowl123@gmail.com>
2025-03-10 23:38:42 -0700

    RayShot Slime fix and small additions.

--- Assets/Skyler_Scripts/CharMovement.cs
+++ Assets/Skyler_Scripts/CharMovement.cs
@@ -7,7 +7,7 @@ public class CharMovement : MonoBehaviour
     public float speed = 7.0f;
     public float jumpHeight = 15.0f; 
     public float gravity = -5.0f;
-
+    [SerializeField] private AudioClip jumpSound;
     private CharacterController control;
     private float airSpeed;
 
@@ -35,7 +35,9 @@ public class CharMovement : MonoBehaviour
 
         if (control.isGrounded) {
             if (Input.GetButtonDown("Jump")) {
+                AudioSource.PlayClipAtPoint(jumpSound, transform.position);
                 airSpeed = jumpHeight;
+
             }
         }
 
--- Assets/Tj-Assets&scripts/Scripts/Updated Scripts/EnemyAI.cs
+++ Assets/Tj-Assets&scripts/Scripts/Updated Scripts/EnemyAI.cs
@@ -7,12 +7,13 @@ public class EnemyAI : MonoBehaviour
 {
     public static event System.Action<GameObject> OnEnemyKilled; // Event to notify when an enemy dies
     public Transform player;
-    public int health = 3;
+    public int health = 1;
     private Rigidbody rigidBody;
     public float moveSpeed = 3.0f;
     public float detectionRadius = 10.0f;
     [SerializeField] private int enemyPoints = 50;
     private GameScore gameScore;
+    [SerializeField] private AudioClip slimeDies;
 
     void Start() {
         player = GameObject.FindWithTag("Player")?.transform;
@@ -30,16 +31,15 @@ public class EnemyAI : MonoBehaviour
     }
 
     public void TakeDamage(int damage) {
-      //  health -= damage;
-        //if (health <= 0) {
-          //  gameScore.hudScore(enemyPoints);
-            //OnEnemyKilled?.Invoke(gameObject); // Notify EnemyManager if subscribed
-            //Destroy(gameObject);
-        //}
+        health -= damage;
+        if (health <= 0) {
+            KillEnemy();
+        }
     }
     public void KillEnemy() {
         OnEnemyKilled?.Invoke(gameObject);
         gameScore.hudScore(enemyPoints);
+        AudioSource.PlayClipAtPoint(slimeDies, transform.position);
         Destroy(gameObject);
     }
 }
